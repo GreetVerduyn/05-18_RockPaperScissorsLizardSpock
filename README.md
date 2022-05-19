861fe6376eb6f7b15f24d9730205f621efd3f453
Greet Verduyn <greetverduyn@proximus.be>
2022-05-19 09:55:34 +0200

    readme and buttons

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # 05-18_RockPaperScissorsLizardSpock
+
+# Title: Rock paper scissors lizard Spock
+Duration: 8 hours
+Learning objectives
+  - Generate random outcomes
+  - If statements
+  - Coming up with a basic algoritm from scratch
+
+## The Mission
+Make a Rock paper scissors lizard Spock. Based on the original game "Rock Paper Scissors" gives you more outcomes and prevents having more than 50% chance of a tie.
+
+### The rules
+- Scissors cut Paper
+- Paper covers Rock
+- Rock crushes Lizard
+- Lizard poisons Spock
+- Spock smashes Scissors
+- Scissors decapitate Lizard
+- Lizard eats Paper
+- Paper disproves Spock
+- Spock vaporizes Rock
+- Rock crushes Scissors
+
+MY START:
+- create an array wit the 5 elements in Javascript
+- make random {choice for computer
+- create 5 buttons in HTML
+- connect buttons with choiceUser
+- make if-statements who wins
+- 
+## Must-have features
+- Have 5 buttons that the player can press to change their pick
+- Have another button to let the computer pick one and show the winner
+- Let the player know they won (no alert box)
+- Have a reset button
+
+## Nice-to-have features
+- Nice graphics
+- Let the player input using a text field
+- Show what the player picked by highlighting the correct button
+- Add even more options (but avoid using 100 if statements)
+- Add complex rules
+
+![You've got this!](http://78.media.tumblr.com/f9247799ae2fe6613f643957020101c6/tumblr_inline_n80n8u8pSz1sbdww6.gif)
--- js/script.js
+++ js/script.js
@@ -1,11 +1,55 @@
-const elements = ["scissors", "paper", "rock", "lizard","spock"];
+const elements = ["scissors", "paper", "rock", "lizard", "spock"];
+let user;
+let computer;
+let userWins = false;
+let userChoice;
+let computerChoice
 
-/*let element1="scissor";
-let element2="paper";
-let element3="rock";
-let element4="lizard";
-let element5="spock";*/
+document.getElementById("computerButton").addEventListener("click", function () {
+    computerChoice = elements[Math.floor(Math.random() * elements.length)];
+})
 
 
 
-console.log(elements[1]);
+let choiceButtons = document.getElementsByClassName("cards");
+console.log(choiceButtons);
+
+for (let i = 0; i < choiceButtons.length; i++) {
+    choiceButtons[i].addEventListener("click", function ()
+    {
+        userChoice = choiceButtons[i].id;
+        console.log(userChoice);
+    })
+};
+
+
+if (computerChoice === userChoice) {
+    document.getElementById("text").innerHTML = `Same cards, try again.`;
+} else {
+    if (userChoice === 'lizard' && (computerChoice === 'paper' || 'spock')) {
+        userWins = true
+    } else {
+        userWins = false
+    }
+    if (userChoice === 'scissors' && (computerChoice === 'paper' || 'lizard')) {
+        userWins = true
+    } else {
+        userWins = false
+    }
+    if (userChoice === 'rock' && (computerChoice === 'lizard' || 'scissors')) {
+        userWins = true
+    } else {
+        userWins = false
+    }
+    if (userChoice === 'spock' && (computerChoice === 'scissors' || 'rock')) {
+        userWins = true
+    } else {
+        userWins = false
+    }
+    if (userChoice === 'paper' && (computerChoice === 'spock' || 'rock')) {
+        userWins = true
+    } else {
+        userWins = false
+    }
+}
+
