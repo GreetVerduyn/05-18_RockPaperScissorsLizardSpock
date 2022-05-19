a48b10f6ee0fce4516d8b2b215582227e5c9adaa
Greet Verduyn <greetverduyn@proximus.be>
2022-05-19 09:53:49 +0200

    choices fixed

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const elements = ["scissors", "paper", "rock", "lizard","spock"];
+
+/*let element1="scissor";
+let element2="paper";
+let element3="rock";
+let element4="lizard";
+let element5="spock";*/
+
+
+
+console.log(elements[1]);
